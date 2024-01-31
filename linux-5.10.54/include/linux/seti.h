41762994b27872897f308483c26e891c6b6eb3ca
Agustin Coitinho <coitinho10@gmail.com>
2024-02-01 00:52:00 +0100

    Syzkaller working with our syscalls but problems with our test programs

---
+++ linux-5.10.54/include/linux/seti.h
@@ -0,0 +1,11 @@
+#ifndef _LINUX_SETI_H
+#define _LINUX_SETI_H
+
+//static int cond_seti;
+
+extern int __sys_seti_1(void);
+extern int __sys_seti_2(int res_seti_1);
+extern int __sys_seti_3(void);
+extern int __sys_seti_4(int * cond_seti_copy);
+
+#endif /*_LINUX_SETI_H */
