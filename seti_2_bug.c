8151e0bb0bc6a08186428b0c9af1074358e54d25
Agustin Coitinho <coitinho10@gmail.com>
2024-02-07 12:10:55 +0100

    Correction to seti_2_bug.c

--- seti_2_bug.c
+++ seti_2_bug.c
@@ -6,7 +6,7 @@
 #include <errno.h>
 
 #define __NR_seti_1 441
-#define __NR_seti_2(int) 442
+#define __NR_seti_2 442
 
 int main (int argc, char *argv[])
 {
@@ -22,7 +22,7 @@ int main (int argc, char *argv[])
 	
 	/* Explicit dependence from seti_sys_2 on seti_sys_1, since
 	the output of seti_sys_1 is a parameter of seti_sys_2 */
-	res_seti_2 = syscall (__NR_seti_2(res_seti_1));
+	res_seti_2 = syscall (__NR_seti_2, res_seti_1);
 	if (res_seti_2 == 0)
 		printf("seti_sys_2 executed\n");
 	else
