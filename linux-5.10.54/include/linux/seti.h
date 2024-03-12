4e22974547a1703fb3b339e70048e764a714c545
Irene <irene.asensio@telecom-paris.fr>
2024-03-12 15:06:23 +0100

    seti.h now contains the macro utilization for the syscalls

--- linux-5.10.54/include/linux/seti.h
+++ linux-5.10.54/include/linux/seti.h
@@ -1,6 +1,7 @@
 #ifndef _LINUX_SETI_H
 #define _LINUX_SETI_H
 
+#include <linux/syscalls.h>
 extern int cond_seti;
 
 extern int __sys_seti_1(void);
@@ -8,4 +9,7 @@ extern int __sys_seti_2(int res_seti_1);
 extern int __sys_seti_3(void);
 extern int __sys_seti_4(int * cond_seti_copy);
 
+SYSCALL_DEFINE1(seti_2, int, res_seti_1){
+  return __sys_seti_2(res_seti_1);
+}
 #endif /*_LINUX_SETI_H */
