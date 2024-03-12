7ecd04b0887492f14ed708d696b684af24260538
Irene <irene.asensio@telecom-paris.fr>
2024-03-12 15:05:50 +0100

    macro SYSCALL_DEFINE has been defined in seti.h so that frama-c does not complain about the macro since even if we preprocess the files for framac, the macro is not resolved yet

--- linux-5.10.54/seti_1/seti_1.c
+++ linux-5.10.54/seti_1/seti_1.c
@@ -1,5 +1,4 @@
 #include <linux/kernel.h>
-#include <linux/syscalls.h>
 #include <linux/seti.h>
 
 /* Initialize the shared variable cond_seti */
--- linux-5.10.54/seti_2/seti_2.c
+++ linux-5.10.54/seti_2/seti_2.c
@@ -1,17 +1,12 @@
 #include <linux/kernel.h>
-#include <linux/syscalls.h>
 #include <linux/seti.h>
 
 int __sys_seti_2(int res_seti_1){
   if (res_seti_1 == 0) {
-    int * vector = NULL;
-	  *vector = 4; /* BOG */
-    return -EFAULT;
+    int * vector = (void *)0;
+    *vector = 4; /* BOG */
+    return 0;
   }
+  
   return 0;
 }
-SYSCALL_DEFINE1(seti_2, int, res_seti_1){
-  return __sys_seti_2(res_seti_1);
-}
-
-
